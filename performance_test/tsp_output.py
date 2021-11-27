afe1e1ed64cced2a06dc73bb09811b7c75aa6759
lglot <luigi@zenbook.localdomain>
2021-11-28 00:41:17 +0100

    fix missing arguments python script

--- performance_test/tsp_output.py
+++ performance_test/tsp_output.py
@@ -33,33 +33,39 @@ def ranking(file, str):
 
 
 if __name__ == "__main__":
-    folder = sys.argv[1]
-    sol = folder+'/output_sol.csv'
-    back = folder+'/output_back.csv'
-    time = folder+'/output_time.csv'
-    with open(sol) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        line_count = 0
-        for row in csv_reader:
-            if line_count == 0:
-                line_count += 1
-            elif line_count == 1:
-                file_op = {(i-2): row[i] for i in range(2, len(row))}
-                line_count += 1
-            else:
-                istance = row[0]
-                pos = 1
-                for word in row[1:]:
-                    if ']' in word:
-                        break
-                    pos += 1
+    try:
+        folder = sys.argv[1]
+    except IndexError:
+        folder = ""
+    if folder == "":
+        print("Argument missing")
+    else:
+        sol = folder+'/output_sol.csv'
+        back = folder+'/output_back.csv'
+        time = folder+'/output_time.csv'
+        with open(sol) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            line_count = 0
+            for row in csv_reader:
+                if line_count == 0:
+                    line_count += 1
+                elif line_count == 1:
+                    file_op = {(i-2): row[i] for i in range(2, len(row))}
+                    line_count += 1
+                else:
+                    istance = row[0]
+                    pos = 1
+                    for word in row[1:]:
+                        if ']' in word:
+                            break
+                        pos += 1
 
-                for i, word in enumerate(row[(pos+1):]):
-                    if word == 'no':
-                        print(
-                            f'Trovata una soluzione diversa per istanza {istance} con file {file_op[i]}')
-                line_count += 1
-        print(f'Processate {line_count} istanze.\n')
+                    for i, word in enumerate(row[(pos+1):]):
+                        if word == 'no':
+                            print(
+                                f'Trovata una soluzione diversa per istanza {istance} con file {file_op[i]}')
+                    line_count += 1
+            print(f'Processate {line_count} istanze.\n')
 
-    ranking(back, "Classifica a punteggio per il numero dei backtracking")
-    ranking(time, "Classifica a punteggio per il tempo di esecuzione")
+        ranking(back, "Classifica a punteggio per il numero dei backtracking")
+        ranking(time, "Classifica a punteggio per il tempo di esecuzione")
